f8beda9a231798b379c3e29a93e2305f5bee5855
sharang joshi <sharangjoshi@sharangs-MacBook-Pro.local>
2024-06-08 16:03:16 +0530

    Added password generator functiuonality

---
+++ src/password-generator.js
@@ -0,0 +1,12 @@
+// script.js
+function generatePassword() {
+    const length = document.getElementById('length').value;
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+<>?";
+
+    let password = "";
+    for (let i = 0, n = charset.length; i < length; ++i) {
+        password += charset.charAt(Math.floor(Math.random() * n));
+    }
+
+    document.getElementById('passwordDisplay').innerText = password;
+}
